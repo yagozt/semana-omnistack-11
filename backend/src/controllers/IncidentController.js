e51ac1572dc65b004ea284c6f830ea792d31c1fa
Yago <yago@aguiabranca.com.br>
2020-03-27 23:41:33 -0300

    Colocando count no header e join no incidents

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -6,10 +6,15 @@ module.exports = {
 
         const [count] = await connection('incidents').count();
 
+        console.log(count);
+
         const incidents = await connection('incidents')
+            .join('ongs', 'ongs.id', '=', 'incidents.ong_id')
             .limit(5)
             .offset((page - 1) * 5)
-            .select('*');
+            .select(['incidents.*', 'ongs.nome', 'ongs.email', 'ongs.whatsapp', 'ongs.city', 'ongs.uf']);
+
+        response.header('X-Total-Count', count['count(*)']);
 
         return response.json(incidents);
     },
